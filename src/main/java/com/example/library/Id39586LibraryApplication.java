1e267cdd7cd0a8d396b22ef5d57e68d0330b8534
id39586 <39586@iitu.edu.kz>
2026-04-01 15:07:43 +0600

    Add application entry points: Id39586LibraryApplication and Id39586Project1FilmsApplication

---
+++ src/main/java/com/example/library/Id39586LibraryApplication.java
@@ -0,0 +1,12 @@
+package com.example.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Id39586LibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Id39586LibraryApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/library/Id39586Project1FilmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Id39586Project1FilmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Id39586Project1FilmsApplication.class, args);
+    }
+
+}
